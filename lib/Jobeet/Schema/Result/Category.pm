00f8dd91ae03537f9722123c0e3e6aa1b21954dc
tamiflu <tamiflu.kyc@gmail.com>
2017-03-23 17:26:04 +0900

    Categoryテーブルにslugカラムを追加

--- lib/Jobeet/Schema/Result/Category.pm
+++ lib/Jobeet/Schema/Result/Category.pm
@@ -4,6 +4,7 @@ use warnings;
 use parent 'Jobeet::Schema::ResultBase';
 
 use Jobeet::Models;
+use String::CamelCase qw(decamelize);
 
 __PACKAGE__->table('jobeet_category');
 
@@ -21,10 +22,16 @@ __PACKAGE__->add_columns(
         size        => 255,
         is_nullable => 0,
     },
+    slug => {
+        data_type   => 'VARCHAR',
+        size        => 255,
+        is_nullable => 1,
+    },
 );
 
 __PACKAGE__->set_primary_key('id');
 __PACKAGE__->add_unique_constraint([ 'name' ]);
+__PACKAGE__->add_unique_constraint([ 'slug' ]);
 __PACKAGE__->has_many( jobs => 'Jobeet::Schema::Result::Job', 'category_id' );
 __PACKAGE__->has_many(
     category_affiliate => 'Jobeet::Schema::Result::CategoryAffiliate', 'category_id',
@@ -48,6 +55,24 @@ sub get_active_jobs {
             row      => $attr->{rows},
         }
     );
+}
+
+sub insert {
+    my $self = shift;
+
+    $self->slug(decamelize $self->name);
+
+    $self->next::method(@_);
+}
+
+sub update {
+    my $self = shift;
+
+    if ($self->is_column_changed('name')) {
+        $self->slug(decamelize $self->name);
+    }
+
+    $self->next::method(@_);
 }
 
 1;
