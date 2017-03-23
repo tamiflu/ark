04d75c003463c32c41dcebbfe9753875cd82af95
tamiflu <tamiflu.kyc@gmail.com>
2017-03-23 18:43:40 +0900

    パジネーション対応

--- lib/Jobeet/Controller/Category.pm
+++ lib/Jobeet/Controller/Category.pm
@@ -12,6 +12,10 @@ sub show :Path :Args(1) {
         or $c->detach('/default');
 
     $c->stash->{category} = $category;
+    $c->stash->{jobs} = $category->get_active_jobs({
+        rows => models('conf')->{max_jobs_on_category},
+        page => $c->req->parameters->{page} || 1,
+    });
 }
 
 1;
--- lib/Jobeet/Schema/Result/Category.pm
+++ lib/Jobeet/Schema/Result/Category.pm
@@ -50,6 +50,7 @@ sub get_active_jobs {
         { expires_at => { '>=', models('Schema')->now->strftime("%F %T") } },
         { order_by => { -desc => 'created_at' },
                 defined $attr->{rows} ? (rows => $attr->{rows}) : (),
+                defined $attr->{page} ? (page => $attr->{page}) : (),
         }
     );
 }
