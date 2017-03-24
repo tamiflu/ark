783fb4ecef26e5809aae04a01e1c2bdb6c01026c
tamiflu <tamiflu.kyc@gmail.com>
2017-03-24 15:01:02 +0900

    各種Jobアクション作成

--- lib/Jobeet/Controller/Job.pm
+++ lib/Jobeet/Controller/Job.pm
@@ -16,6 +16,9 @@ sub index :Path {
 # /job/{job_token} (詳細)
 sub show :Path :Args(1) {
     my ($self, $c, $job_token) = @_;
+
+    $c->stash->{job} = models('Schema::Job')->find({ token => $job_token })
+        or $c->detach('/default');
 }
 
 # /job/create (新規作成)
@@ -23,21 +26,46 @@ sub create :Local :Form('Jobeet::Form::Job') {
     my ($self, $c) = @_;
 
     $c->stash->{form} = $self->form;
+    if ($c->req->method eq 'POST' and $self->form->submitted_and_valid) {
+        my $job = models('Schema::Job')->create_from_form($self->form);
+        $c->redirect( $c->uri_for('/job', $job->token) );
+    }
 }
 
+# /{job_token}/job
 sub job :Chained('/') :PathPart :CaptureArgs(1) {
     my ($self, $c, $job_token) = @_;
-    $c->stash->{job_token} = $job_token;
+
+    $c->stash->{job} = models('Schema::Job')->find({ token => $job_token })
+        or $c->detach('/default');
 }
 
 # /job/{job_token}/edit (編集)
-sub edit :Chained('job') :PathPart :Args(0) {
+sub edit :Chained('job') :PathPart :Form('Jobeet::Form::Job') {
     my ($self, $c) = @_;
+
+    my $job = $c->stash->{job};
+
+    if ($c->req->method eq 'POST') {
+        if ($self->form->submitted_and_valid) {
+            $job->update_from_form($self->form);
+            $c->redirect( $c->uri_for('/job', $job->token) );
+        }
+    }
+    else {
+        $self->form->fill({
+            $job->get_columns,
+            category => $job->category->slug,
+        });
+    }
 }
 
 # /job/{job_token}/delete (削除)
-sub delete :Chained('job') :PathPart :Args(0) {
+sub delete :Chained('job') :PathPart {
     my ($self, $c) = @_;
+
+    $c->stash->{job}->delete;
+    $c->redirect( $c->uri_for('/job') );
 }
 
 1;
--- lib/Jobeet/Schema/ResultSet/Job.pm
+++ lib/Jobeet/Schema/ResultSet/Job.pm
@@ -25,4 +25,23 @@ sub get_active_jobs {
     $self;
 }
 
+sub create_from_form {
+    my ($self, $form) = @_;
+
+    my $txn_guard = models('Schema')->txn_scope_guard;
+
+    my $category_id = delete $form->params->{category};
+    my $category = models('Schema::Category')->find({ slug => $category_id })
+        or die 'no such category_id: ', $category_id;
+
+    my $job = $self->create({
+        category_id => $category->id,
+        %{$form->params},
+    });
+
+    $txn_guard->commit;
+
+    $job;
+}
+
 1;
