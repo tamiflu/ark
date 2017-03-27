83d9692d6b9e23ccda47ba5d65f924fb66c49e46
tamiflu <tamiflu.kyc@gmail.com>
2017-03-27 12:02:39 +0900

    APIを追加

--- cpanfile
+++ cpanfile
@@ -14,3 +14,4 @@ requires 'Data::Page::Navigation';
 requires 'CGI::Simple';
 requires 'Cache::FastMmap';
 requires 'DateTime::Format::W3CDTF';
+requires 'JSON::Any';
--- lib/Jobeet/Schema/Result/Affiliate.pm
+++ lib/Jobeet/Schema/Result/Affiliate.pm
@@ -4,6 +4,8 @@ use strict;
 use warnings;
 use parent 'Jobeet::Schema::ResultBase';
 use Jobeet::Schema::Types;
+use Digest::SHA1 qw/sha1_hex/;
+use Data::UUID;
 
 __PACKAGE__->table('jobeet_affiliate');
 
@@ -22,5 +24,14 @@ __PACKAGE__->set_primary_key('id');
 __PACKAGE__->add_unique_constraint([ 'email' ]);
 __PACKAGE__->has_many(category_affiliate => 'Jobeet::Schema::Result::CategoryAffiliate', 'affiliate_id' );
 __PACKAGE__->many_to_many( categories => category_affiliate => 'category' );
+
+sub insert {
+    my $self = shift;
+
+    $self->token(sha1_hex(Data::UUID->new->create))
+        unless $self->token;
+
+    $self->next::method(@_);
+}
 
 1;
